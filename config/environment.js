8c01e88e36a5a30be2a1165131ac522af326ea22
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2011-09-26 12:05:11 +0400

    Remove mongo

--- config/environment.js
+++ config/environment.js
@@ -1,14 +1,4 @@
-var mongoStore = require('connect-mongodb');
 var express = require('express');
-
-app.settings.db = JSON.parse(require('fs').readFileSync(__dirname + '/database.json', 'utf-8'))[app.settings.env];
-
-var mongoSessionStore = new mongoStore({
-    dbname:   app.settings.db.database,
-    host:     app.settings.db.host,
-    username: app.settings.db.user,
-    password: app.settings.db.password
-}, function () {});
 
 app.configure(function(){
     app.use(express.static(app.root + '/public'));
@@ -17,7 +7,7 @@ app.configure(function(){
     app.set('defaultLocale', 'en');
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-    app.use(express.session({secret: 'secret', store: mongoSessionStore}));
+    app.use(express.session({secret: 'secret'}));
     app.use(express.methodOverride());
     app.use(app.router);
 });
