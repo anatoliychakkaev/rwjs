936c68a3200ce7cf32619438ad3c1228eb25e8dd
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-02-01 23:43:40 +0400

    Removed railway

--- node_modules/railway/templates/server.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var app = module.exports = require('railway').createServer();
-
-if (!module.parent) {
-    app.listen(process.env.PORT || 3000);
-    console.log("Railway server listening on port %d within %s environment", app.address().port, app.settings.env);
-}
-
