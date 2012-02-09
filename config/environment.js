89215b326e24cb050389cf0ef09d9e2145a3aceb
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-02-10 01:58:19 +0400

    Turn logging on

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 
+require('railway-monitor').init(app, {
+    appKey: '52bd4bb6-0b87-4d59-b46c-7a921b1abbe6',
+    host: 'node-js.ru',
+    port: 1678
+});
+
 app.configure(function(){
     app.use(express.static(app.root + '/public'));
     app.set('views', app.root + '/app/views');
